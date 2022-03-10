f3f22061a76cc6ff00c22ad2b61f1080c79b7693
Yichai-4 <hazanyichai@gmail.com>
2022-03-10 15:24:53 +0200

    Beginning Exercise 1

---
+++ Exercise_1/Exercise_1.go
@@ -0,0 +1,70 @@
+/*
+Submitter 1: Yichaï Hazan
+ID: 1669535
+Submitter 2: Amitai Shmeeda
+ID: 305361479
+Group number: 150060.01.5782.41-42
+*/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// Receive the path in program argument
+var path = os.Args[1] // receiving the path as cli parameter
+var pathArray = strings.Split(path, "\\")
+
+// Create the output file with the according name
+var directoryName = pathArray[len(pathArray)-1]
+var outputFile, _ = os.Create(directoryName + ".asm")
+
+func main() {
+	// Close the file "outputFile" at the end of the main function
+	defer outputFile.Close()
+
+	// Go through the file and performs some operations
+	// filepath.WalkDir()
+	filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		fileName := info.Name()
+		extension := filepath.Ext(fileName)
+		if extension == ".vm" {
+			fmt.Printf("File Name: %s\n", fileName)
+			// removes the extension from the file name and prints it
+			name := strings.TrimRight(fileName, extension)
+			outputFile.WriteString(name + "\n")
+
+			inputFile, err := os.Open(path)
+			check(err)
+			defer inputFile.Close()
+
+			scanner := bufio.NewScanner(inputFile)
+
+			for scanner.Scan() {
+
+			}
+
+			if err := scanner.Err(); err != nil {
+				log.Fatal(err)
+			}
+
+		}
+		return nil
+	})
+
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
