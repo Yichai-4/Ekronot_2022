d1d1f08a95536b2310c1865274cc9a0720915042
Yichai-4 <hazanyichai@gmail.com>
2022-03-24 15:44:32 +0200

    Finishes Exercise 1 All tests ended successfully

--- Exercise_1/Exercise_1.go
+++ Exercise_1/Exercise_1.go
@@ -14,17 +14,21 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
 // Receive the path in program argument
-// var path = os.Args[1] // receiving the path as cli parameter
-var path = "C:\\Ekronot_2022\\nand2tetris\\projects\\07\\MemoryAccess\\BasicTest"
+var path = os.Args[1] // receiving the path as cli parameter
+//var path = "C:\\Ekronot_2022\\nand2tetris\\projects\\07\\MemoryAccess\\StaticTest"
+
 var pathArray = strings.Split(path, "\\")
 
 // Create the output file with the according name
 var fileName = pathArray[len(pathArray)-1]
 var outputFile, _ = os.Create(fileName + ".asm")
+
+var labelCount = 0
 
 func main() {
 	// Close the file "outputFile" at the end of the main function
@@ -54,19 +58,13 @@ func main() {
 				command := words[0]
 				switch command {
 				// Arithmetic commands
-				case "add":
-				case "sub":
-				case "neg":
+				case "add", "sub", "neg":
 					WriteArithmetic(command)
 				// Boolean commands
-				case "eq": // Equality
-				case "gt": // Greater than
-				case "lt": // Less than
+				case "eq", "gt", "lt":
 					WriteBoolean(command)
 				// Logical commands - Bit-wise
-				case "and":
-				case "or":
-				case "not":
+				case "and", "or", "not":
 					WriteLogical(command)
 				// Memory access commands
 				case "pop":
@@ -103,7 +101,7 @@ func WriteArithmetic(command string) {
 		outputFile.WriteString("@SP\nM=M-1\nA=M\nD=M\n@result\nM=D-M\n") // SP--, result=x(*SP)-y
 	case "neg": // Arithmetic negation (2's complement)
 		outputFile.WriteString("// neg\n")
-		outputFile.WriteString("@SP\nM=M-1\nA=M\nD=M\n@result\nM=M-D\n") // SP--, result=0-x
+		outputFile.WriteString("@SP\nM=M-1\nA=M\nD=M\n@result\nM=0\nM=M-D\n") // SP--, result=0-x
 	}
 	outputFile.WriteString("@result\nD=M\n@SP\nA=M\nM=D\n") // *SP=result
 	outputFile.WriteString("@SP\nM=M+1\n")                  // SP++
@@ -111,31 +109,32 @@ func WriteArithmetic(command string) {
 
 // WriteBoolean Translation of boolean command (i.e. eq, gt or lt) in VM language to Hack language
 func WriteBoolean(command string) {
+	labelCount = labelCount + 1
+	labelCountStr := strconv.Itoa(labelCount)
 	switch command {
 	case "eq": // Equality
 		outputFile.WriteString("// eq\n")
-		outputFile.WriteString("@SP\nM=M-1\nA=M\nD=M\n") // SP--, D=STACK[SP]
-		outputFile.WriteString("@SP\nA=M-1\nD=D-M\n")    // D=D-STACK[SP-1]
-		outputFile.WriteString("@IF_TRUE\nD;JEQ\n")      // jump to (IF_TRUE) if D=0
+		outputFile.WriteString("@SP\nM=M-1\nA=M\nD=M\n")                  // SP--, D=STACK[SP]
+		outputFile.WriteString("@SP\nA=M-1\nD=M-D\n")                     // D=D-STACK[SP-1]
+		outputFile.WriteString("@IF_TRUE_" + labelCountStr + "\nD;JEQ\n") // jump to (IF_TRUE) if D=0
 	case "gt": // Greater than
 		outputFile.WriteString("// gt\n")
-		outputFile.WriteString("@SP\nM=M-1\nA=M\nD=M\n") // SP--, D=STACK[SP]
-		outputFile.WriteString("@SP\nA=M-1\nD=D-M\n")    // D=D-STACK[SP-1]
-		outputFile.WriteString("@IF_TRUE\nD;JGT\n")      // jump to (IF_TRUE) if D>0
+		outputFile.WriteString("@SP\nM=M-1\nA=M\nD=M\n")                  // SP--, D=STACK[SP]
+		outputFile.WriteString("@SP\nA=M-1\nD=M-D\n")                     // D=D-STACK[SP-1]
+		outputFile.WriteString("@IF_TRUE_" + labelCountStr + "\nD;JGT\n") // jump to (IF_TRUE) if D>0
 	case "lt": // Less than
 		outputFile.WriteString("// lt\n")
-		outputFile.WriteString("@SP\nM=M-1\nA=M\nD=M\n") // SP--, D=STACK[SP]
-		outputFile.WriteString("@SP\nA=M-1\nD=D-M\n")    // D=D-STACK[SP-1]
-		outputFile.WriteString("@IF_TRUE\nD;JLT\n")      // jump to (IF_TRUE) if D<0
+		outputFile.WriteString("@SP\nM=M-1\nA=M\nD=M\n")                  // SP--, D=STACK[SP]
+		outputFile.WriteString("@SP\nA=M-1\nD=M-D\n")                     // D=D-STACK[SP-1]
+		outputFile.WriteString("@IF_TRUE_" + labelCountStr + "\nD;JLT\n") // jump to (IF_TRUE) if D<0
 	}
 	// If the condition is not met
-	outputFile.WriteString("@SP\nA=M-1\nM=0\n") // *SP=0
-	outputFile.WriteString("@IF_FALSE\n")       // LABEL
-	outputFile.WriteString("0 ; JMP\n")         // unconditional jump
+	outputFile.WriteString("@SP\nA=M-1\nM=0\n")                        // *SP=0
+	outputFile.WriteString("@IF_FALSE_" + labelCountStr + "\n0;JMP\n") // unconditional jump to (IF_FALSE)
 	// Otherwise
-	outputFile.WriteString("(IF_TRUE)\n")        // Declaring a label
-	outputFile.WriteString("@SP\nA=M-1\nM=-1\n") // *SP=-1
-	outputFile.WriteString("(IF_FALSE)\n")       // Declaring a label
+	outputFile.WriteString("(IF_TRUE_" + labelCountStr + ")\n")  // declaring a label
+	outputFile.WriteString("@SP\nA=M-1\nM=-1\n")                 // *SP=-1
+	outputFile.WriteString("(IF_FALSE_" + labelCountStr + ")\n") // declaring a label
 }
 
 // WriteLogical Translation of logical command (i.e. and, or and not) in VM language to Hack language
@@ -148,7 +147,7 @@ func WriteLogical(command string) {
 	case "or":
 		outputFile.WriteString("// or\n")
 		outputFile.WriteString("@SP\nM=M-1\nA=M\nD=M\n") // D=STACK[SP]
-		outputFile.WriteString("A=A-1\nM=D||M\n")        // STACK[SP]=x or y
+		outputFile.WriteString("A=A-1\nM=D|M\n")         // STACK[SP]=x or y
 	case "not":
 		outputFile.WriteString("// not\n")
 		outputFile.WriteString("@SP\nA=M-1\nM=!M\n") // STACK[SP]=not(x)
@@ -180,7 +179,7 @@ func WritePop(segment string, i string) {
 		outputFile.WriteString("@SP\nM=M-1\n")                       // SP--
 		outputFile.WriteString("@SP\nA=M\nD=M\n")                    // D=*SP
 		outputFile.WriteString("@" + fileName + "." + i + "\nM=D\n") // static i = D
-		os.Exit(0)
+		return
 	// Translation for the command pop temp i
 	case "temp":
 		outputFile.WriteString("@" + i + "\nD=A\n@5\nD=D+A\n@addr\nM=D\n") // addr=5+i
@@ -189,9 +188,9 @@ func WritePop(segment string, i string) {
 	case "pointer":
 		outputFile.WriteString("@SP\nM=M-1\n") // SP--
 		if i == "0" {
-			outputFile.WriteString("@THIS\nD=M\n@addr\nM=D\n") // addr=THIS
+			outputFile.WriteString("@THIS\nD=A\n@addr\nM=D\n") // addr=THIS
 		} else { // i == "1"
-			outputFile.WriteString("@THAT\nD=M\n@addr\nM=D\n") // addr=THAT
+			outputFile.WriteString("@THAT\nD=A\n@addr\nM=D\n") // addr=THAT
 		}
 	}
 	// For all pop commands (except for static) add the value to the according address:
